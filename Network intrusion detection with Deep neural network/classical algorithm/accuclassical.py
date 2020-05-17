0ec66b375f51c083cd6111e65257f9b1041f9005
jagadeesh1099 <60669143+jagadeesh1099@users.noreply.github.com>
2020-05-17 15:01:17 -0700

    Commit final

---
+++ Network intrusion detection with Deep neural network/classical algorithm/accuclassical.py
@@ -0,0 +1,194 @@
+
+
+
+
+import pandas as pd
+from sklearn.metrics import (precision_score, recall_score,f1_score, accuracy_score,mean_squared_error,mean_absolute_error)
+
+
+
+traindata = pd.read_csv('expectedsys.txt', header=None)
+
+testdata = pd.read_csv('predictedlabelABsys.txt', header=None)
+
+
+
+y_train1 = traindata
+y_pred = testdata
+accuracy = accuracy_score(y_train1, y_pred)
+recall = recall_score(y_train1, y_pred , average="binary")
+precision = precision_score(y_train1, y_pred , average="binary")
+f1 = f1_score(y_train1, y_pred, average="binary")
+
+print("ABresults")
+print("accuracy")
+print("%.3f" %accuracy)
+print("precision")
+print("%.3f" %precision)
+print("racall")
+print("%.3f" %recall)
+print("f1score")
+print("%.3f" %f1)
+
+
+testdata = pd.read_csv('predictedlabelDTsys.txt', header=None)
+
+
+
+y_train1 = traindata
+y_pred = testdata
+accuracy = accuracy_score(y_train1, y_pred)
+recall = recall_score(y_train1, y_pred , average="binary")
+precision = precision_score(y_train1, y_pred , average="binary")
+f1 = f1_score(y_train1, y_pred, average="binary")
+
+print("DTresults")
+print("accuracy")
+print("%.3f" %accuracy)
+print("precision")
+print("%.3f" %precision)
+print("racall")
+print("%.3f" %recall)
+print("f1score")
+print("%.3f" %f1)
+
+
+testdata = pd.read_csv('predictedlabelKNN.txt', header=None)
+
+
+
+y_train1 = traindata
+y_pred = testdata
+accuracy = accuracy_score(y_train1, y_pred)
+recall = recall_score(y_train1, y_pred , average="binary")
+precision = precision_score(y_train1, y_pred , average="binary")
+f1 = f1_score(y_train1, y_pred, average="binary")
+
+print("KNNresults")
+print("accuracy")
+print("%.3f" %accuracy)
+print("precision")
+print("%.3f" %precision)
+print("racall")
+print("%.3f" %recall)
+print("f1score")
+print("%.3f" %f1)
+
+
+
+testdata = pd.read_csv('predictedlabelLRsys.txt', header=None)
+
+
+
+y_train1 = traindata
+y_pred = testdata
+accuracy = accuracy_score(y_train1, y_pred)
+recall = recall_score(y_train1, y_pred , average="binary")
+precision = precision_score(y_train1, y_pred , average="binary")
+f1 = f1_score(y_train1, y_pred, average="binary")
+
+print("LRresults")
+print("accuracy")
+print("%.3f" %accuracy)
+print("precision")
+print("%.3f" %precision)
+print("racall")
+print("%.3f" %recall)
+print("f1score")
+print("%.3f" %f1)
+
+
+
+testdata = pd.read_csv('predictedlabelNBsys.txt', header=None)
+
+
+
+y_train1 = traindata
+y_pred = testdata
+accuracy = accuracy_score(y_train1, y_pred)
+recall = recall_score(y_train1, y_pred , average="binary")
+precision = precision_score(y_train1, y_pred , average="binary")
+f1 = f1_score(y_train1, y_pred, average="binary")
+
+print("NBresults")
+print("accuracy")
+print("%.3f" %accuracy)
+print("precision")
+print("%.3f" %precision)
+print("racall")
+print("%.3f" %recall)
+print("f1score")
+print("%.3f" %f1)
+
+
+
+testdata = pd.read_csv('predictedlabelRFsys.txt', header=None)
+
+
+
+y_train1 = traindata
+y_pred = testdata
+accuracy = accuracy_score(y_train1, y_pred)
+recall = recall_score(y_train1, y_pred , average="binary")
+precision = precision_score(y_train1, y_pred , average="binary")
+f1 = f1_score(y_train1, y_pred, average="binary")
+
+print("RFresults")
+print("accuracy")
+print("%.3f" %accuracy)
+print("precision")
+print("%.3f" %precision)
+print("racall")
+print("%.3f" %recall)
+print("f1score")
+print("%.3f" %f1)
+
+
+
+testdata = pd.read_csv('predictedlabelSVM-linearsys.txt', header=None)
+
+
+
+y_train1 = traindata
+y_pred = testdata
+accuracy = accuracy_score(y_train1, y_pred)
+recall = recall_score(y_train1, y_pred , average="binary")
+precision = precision_score(y_train1, y_pred , average="binary")
+f1 = f1_score(y_train1, y_pred, average="binary")
+
+print("SVM-linearresults")
+print("accuracy")
+print("%.3f" %accuracy)
+print("precision")
+print("%.3f" %precision)
+print("racall")
+print("%.3f" %recall)
+print("f1score")
+print("%.3f" %f1)
+
+
+
+testdata = pd.read_csv('predictedlabelSVM-rbfsys.txt', header=None)
+
+
+
+y_train1 = traindata
+y_pred = testdata
+accuracy = accuracy_score(y_train1, y_pred)
+recall = recall_score(y_train1, y_pred , average="binary")
+precision = precision_score(y_train1, y_pred , average="binary")
+f1 = f1_score(y_train1, y_pred, average="binary")
+
+print("SVM-rbfresults")
+print("accuracy")
+print("%.3f" %accuracy)
+print("precision")
+print("%.3f" %precision)
+print("racall")
+print("%.3f" %recall)
+print("f1score")
+print("%.3f" %f1)
+
+
+
+
